fd97a6925aa40edb2729b1e9ff6530a9abfb7990
Luke <lukemcdonnl@gmail.com>
2020-01-21 21:59:20 +0930

    test local socket url

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import {SocketIoConfig, SocketIoModule} from "ngx-socket-io";
 import { HomeComponent } from './routes/home/home.component';
 import {Ng5SliderModule} from "ng5-slider";
 
-const config: SocketIoConfig = { url: 'http://volumio', options: {} };
+const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
 
 @NgModule({
   declarations: [
