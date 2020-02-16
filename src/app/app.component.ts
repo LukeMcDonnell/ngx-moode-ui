fe7f7cfd39793a20825d618194745ecc6618facf
Luke <lukemcdonnl@gmail.com>
2020-02-16 15:51:12 +0930

    responsive now playing screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import {Component, HostListener} from '@angular/core';
-import {SocketService} from "./service/socket.service";
-import {Observable} from "rxjs";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,30 +6,13 @@ import {Observable} from "rxjs";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  @HostListener('window:scroll', ['$event']) onScrollEvent($event){
-    //TODO: Fix this for firefox & other browsers
-    this.scrolled = $event.path[1].scrollY > 50;
-  }
 
-  public state: Observable<any>;
   public scrolled = false;
 
-  constructor(public socketService: SocketService) {
-    this.state = this.socketService.getMessages('pushState');
-  }
+  constructor() {}
 
-  public getAlbumArt(albumart:string): string {
-    if (!albumart) {
-      // TODO: return a default image here?
-      return null;
-    }
-
-    if (albumart.charAt(0) === '/') {
-      //todo use socket hostname here
-      return this.socketService.host + albumart
-    }
-
-    return albumart;
+  public onScroll($event){
+    this.scrolled = $event.target.scrollTop > 50;
   }
 
 }
